365ac7cef60aa95fafd63cb6d1170a8d3c6433f1
Marcus Petit <marcuspetit@hotmail.com>
2024-04-26 05:50:16 -0300

    Buscar pelo title do post por query

--- src/main/java/com/example/mongodb/mongodb/repository/PostRepository.java
+++ src/main/java/com/example/mongodb/mongodb/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.example.mongodb.mongodb.repository;
 
 import com.example.mongodb.mongodb.entitides.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,10 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title' : { $regex: ?0, $options: 'i' }}")
+    List<Post> resultTitle (String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
